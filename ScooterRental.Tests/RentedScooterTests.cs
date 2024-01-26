bc7c970be91db0c7f8afbbb66cdae9f84972bc2e
brueqlv <gundarens@gmail.com>
2024-01-26 16:18:24 +0200

    RentedScooter tests done

---
+++ ScooterRental.Tests/RentedScooterTests.cs
@@ -0,0 +1,38 @@
+﻿
+using FluentAssertions;
+
+namespace ScooterRental.Tests
+{
+    public class RentedScooterTests
+    {
+
+        private string _id = "1";
+        private DateTime _startDate = new DateTime(2024, 01, 01, 00, 00, 00);
+        private decimal _pricePerMinute = 0.2m;
+
+        [Test]
+        public void Construcor_Should_Set_ScooterId_RentStart_PricePerMinute_Correctly()
+        {
+            var rentedScooter = new RentedScooter(_id, _startDate, _pricePerMinute);
+
+            //Assert
+            rentedScooter.ScooterId.Should().Be(_id);
+            rentedScooter.RentStart.Should().Be(_startDate);
+            rentedScooter.PricePerMinute.Should().Be(_pricePerMinute);
+        }
+
+        [Test]
+        public void Property_RentEnd_Should_Set_RentEnd_Correctly()
+        {
+            //Arrange
+            DateTime rentEnd = new DateTime(2024, 01, 02, 10, 00, 00);
+            var rentedScooter = new RentedScooter(_id, _startDate, _pricePerMinute);
+
+            //Act
+            rentedScooter.RentEnd = rentEnd;
+
+            //Assert
+            rentedScooter.RentEnd.Should().Be(rentEnd);
+        }
+    }
+}
